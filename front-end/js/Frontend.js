f5fe0cddab1a979309d64a05139e9875d292a7ac
ckk <takenheart7@gmail.com>
2023-10-21 17:08:27 -0500

    Sat commit 2
    Split into front and back end

---
+++ front-end/js/Frontend.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+
+}
+
+function submitActionListener() {
+    console.log('form submitted');
+    
+}
+
+document.getElementById('btnRegister').addEventListener('click', () => {
+    submitActionListener();
+});
